e48afa931d65aaf9da36f1d89cd5c9c4073648dd
guohaoyuan <2671584689@qq.com>
2020-12-01 00:34:41 +0800

    fix uint8->string->time.Time

--- server/Main.go
+++ server/Main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"time"
 )
 
 var Db *sql.DB
@@ -29,15 +30,15 @@ func checkErr(err error) {
 }
 
 type User struct {
-	id int `db:"id"`
-	openid string `db:"openid"`
-	sessionKey string `db:"session_key"`
-	age int `db:"age"`
-	gender int `db:"gender"`
-	registerTime []uint8 `db:"register_time"`
-	lastLoginTime []uint8 `db:"lastLogin_time"`
-	nickName string `db:"nickname"`
-	birthday []uint8 `db:"birthday"`
+	id            int       `db:"id"`
+	openid        string    `db:"openid"`
+	sessionKey    string    `db:"session_key"`
+	age           int       `db:"age"`
+	gender        int       `db:"gender"`
+	registerTime  time.Time `db:"register_time"`
+	lastLoginTime time.Time `db:"lastLogin_time"`
+	nickName      string    `db:"nickname"`
+	birthday      time.Time `db:"birthday"`
 }
 
 func main() {
@@ -45,24 +46,30 @@ func main() {
 	r.POST("/api/getuserinfo", func(c *gin.Context) {
 		user := User{}
 		id := c.PostForm("userid")
-		sql :="select * from user where id = ?;"
-		stmt,err:=Db.Prepare(sql)
+		sql := "select * from user where id = ?;"
+		stmt, err := Db.Prepare(sql)
 		checkErr(err)
 		defer stmt.Close()
 		row := stmt.QueryRow(id)
-		err = row.Scan(&user.id, &user.openid, &user.sessionKey, &user.age, &user.gender, &user.registerTime,
-			&user.lastLoginTime, &user.nickName, &user.birthday)
+		var registerTime string
+		var lastLoginTime string
+		var birthday string
+		err = row.Scan(&user.id, &user.openid, &user.sessionKey, &user.age, &user.gender, &registerTime,
+			&lastLoginTime, &user.nickName, &birthday)
 		checkErr(err)
+		user.registerTime,_ = time.Parse("2006-01-02 15:04:05",registerTime)
+		user.lastLoginTime,_ = time.Parse("2006-01-02 15:04:05",lastLoginTime)
+		user.birthday,_ = time.Parse("2006-01-02 15:04:05",birthday)
 		c.JSON(200, gin.H{
-			"id": user.id,
-			"openid":user.openid,
-			"session_key":user.sessionKey,
-			"age":user.age,
-			"gender":user.gender,
-			"register_time":user.registerTime,
-			"lastLoginTime":user.lastLoginTime,
-			"nickname":user.nickName,
-			"birthday":user.birthday,
+			"id":            user.id,
+			"openid":        user.openid,
+			"session_key":   user.sessionKey,
+			"age":           user.age,
+			"gender":        user.gender,
+			"register_time": user.registerTime,
+			"lastLoginTime": user.lastLoginTime,
+			"nickname":      user.nickName,
+			"birthday":      user.birthday,
 		})
 	})
 	r.GET("/pings", func(c *gin.Context) {
