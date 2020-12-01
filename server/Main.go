3ae1ca75690f3aa9ac56ed6b1c3a615a48a62ac0
guohaoyuan <2671584689@qq.com>
2020-12-01 13:52:45 +0800

    用户注册/api/user/register (test)   fix 时差 数据库date和datetime

--- server/Main.go
+++ server/Main.go
@@ -12,9 +12,10 @@ import (
 var Db *sql.DB
 
 func init() {
+	fmt.Println(time.Now())
 	fmt.Println("系统启动的时候就加载(并不是用户访问的时候加载) 因为连接池 只是用户使用的时候就会建立连接 用完返回")
 	var err error
-	Db, err = sql.Open("mysql", "pass:pass@tcp(47.102.101.77:3306)/PASSGO")
+	Db, err = sql.Open("mysql", "pass:pass@tcp(47.102.101.77:3306)/PASSGO?charset=utf8mb4&parseTime=true&loc=Local")
 	if err != nil {
 		log.Panicln("err:", err.Error())
 	}
@@ -43,37 +44,57 @@ type User struct {
 
 func main() {
 	r := gin.Default()
-	r.POST("/api/getuserinfo", func(c *gin.Context) {
-		user := User{}
-		id := c.PostForm("userid")
-		sql := "select * from user where id = ?;"
-		stmt, err := Db.Prepare(sql)
-		checkErr(err)
-		defer stmt.Close()
-		row := stmt.QueryRow(id)
-		var registerTime string
-		var lastLoginTime string
-		var birthday string
-		err = row.Scan(&user.id, &user.openid, &user.sessionKey, &user.age, &user.gender, &registerTime,
-			&lastLoginTime, &user.nickName, &birthday)
-		checkErr(err)
-		user.registerTime,_ = time.Parse("2006-01-02 15:04:05",registerTime)
-		user.lastLoginTime,_ = time.Parse("2006-01-02 15:04:05",lastLoginTime)
-		user.birthday,_ = time.Parse("2006-01-02 15:04:05",birthday)
-		c.JSON(200, gin.H{
-			"id":            user.id,
-			"openid":        user.openid,
-			"session_key":   user.sessionKey,
-			"age":           user.age,
-			"gender":        user.gender,
-			"register_time": user.registerTime,
-			"lastLoginTime": user.lastLoginTime,
-			"nickname":      user.nickName,
-			"birthday":      user.birthday,
-		})
-	})
-	r.GET("/pings", func(c *gin.Context) {
-		fmt.Println("Hello, World2!")
-	})
+	// 获取用户信息 post:{"userid":xx}
+	r.POST("/api/user/getbyid", getUserById)
+	// 用户注册 post: 注册时间和上次登录时间直接用time.Now()
+	//post = {
+	//    "openid": "",
+	//    "session_key": "",
+	//    "age": ,
+	//    "gender": ,
+	//    "nickname": "用户名",
+	//    "birthday": "2020-01-01",
+	//}
+	r.POST("/api/user/register", registerUser)
+
 	r.Run(":23333")
+}
+
+func getUserById(c *gin.Context) {
+	user := User{}
+	id := c.PostForm("userid")
+	sql := "select * from user where id = ?;"
+	stmt, err := Db.Prepare(sql)
+	checkErr(err)
+	defer stmt.Close()
+	row := stmt.QueryRow(id)
+	var registerTime string
+	var lastLoginTime string
+	var birthday string
+	err = row.Scan(&user.id, &user.openid, &user.sessionKey, &user.age, &user.gender, &registerTime,
+		&lastLoginTime, &user.nickName, &birthday)
+	checkErr(err)
+	user.registerTime, _ = time.Parse("2006-01-02 15:04:05", registerTime)
+	user.lastLoginTime, _ = time.Parse("2006-01-02 15:04:05", lastLoginTime)
+	user.birthday, _ = time.Parse("2006-01-02", birthday)
+	c.JSON(200, gin.H{
+		"id":            user.id,
+		"openid":        user.openid,
+		"session_key":   user.sessionKey,
+		"age":           user.age,
+		"gender":        user.gender,
+		"register_time": user.registerTime,
+		"lastLoginTime": user.lastLoginTime,
+		"nickname":      user.nickName,
+		"birthday":      user.birthday,
+	})
+}
+
+func registerUser(c *gin.Context) {
+	sql := "insert into user(openid,session_key,age,gender,register_time,last_login_time,nickname,birthday)" +
+		" values(?,?,?,?,?,?,?,?)"
+	res,err:= Db.Exec(sql,c.PostForm("openid"),c.PostForm("session_key"),c.PostForm("age"),
+		c.PostForm("gender"),time.Now(),time.Now(),c.PostForm("nickname"),c.PostForm("birthday"))
+	checkErr(err)
+	fmt.Println(res.LastInsertId())
 }
