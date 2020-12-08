19849413ae946144211a6ef41b9d658536dc4211
章一砚 <1052886775@qq.com>
2020-12-08 13:39:21 +0800

    isOK 检验棋盘是否足够聚拢，成功

--- wxapp/pages/zyy_memory_span/memory_span.js
+++ wxapp/pages/zyy_memory_span/memory_span.js
@@ -1,21 +1,21 @@
 const app = getApp();
 const board_size = 16;
 Page({
-  /**
-   * 页面的初始数据
+  /**                                                                        
+   * 页面的初始数据                                                                        
    */
   data: {
-    difficulty: 7 /* [ 5、6、7 ] */,
-    board_num: [] /* board.length = board_size */,
+    difficulty: 7 /* [ 5、6、7 ] */ ,
+    board_num: [] /* board.length = board_size */ ,
     board_img_url: [],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
+  /**                                                                        
+   * 生命周期函数--监听页面加载                                                                        
    */
   onLoad: function (options) {
     let tar = this.data;
-    tar.board_num = get_Random_board(tar.difficulty,16);
+    tar.board_num = get_Random_board(tar.difficulty, board_size);
     tar.board_img_url = [];
     tar.board_num.forEach((e) => {
       tar.board_img_url.push(get_num_img(e));
@@ -23,56 +23,56 @@ Page({
     console.log(tar.board_num);
     console.log(tar.board_img_url);
     this.setData({
-      board_num:tar.board_num,
-      board_img_url:tar.board_img_url
+      board_num: tar.board_num,
+      board_img_url: tar.board_img_url
     });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
+  /**                                                                        
+   * 生命周期函数--监听页面初次渲染完成                                                                        
    */
   onReady: function () {},
 
-  /**
-   * 生命周期函数--监听页面显示
+  /**                                                                        
+   * 生命周期函数--监听页面显示                                                                        
    */
   onShow: function () {},
 
-  /**
-   * 生命周期函数--监听页面隐藏
+  /**                                                                        
+   * 生命周期函数--监听页面隐藏                                                                        
    */
   onHide: function () {},
 
-  /**
-   * 生命周期函数--监听页面卸载
+  /**                                                                        
+   * 生命周期函数--监听页面卸载                                                                        
    */
   onUnload: function () {},
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
+  /**                                                                        
+   * 页面相关事件处理函数--监听用户下拉动作                                                                        
    */
   onPullDownRefresh: function () {},
 
-  /**
-   * 页面上拉触底事件的处理函数
+  /**                                                                        
+   * 页面上拉触底事件的处理函数                                                                        
    */
   onReachBottom: function () {},
 
-  /**
-   * 用户点击右上角分享
+  /**                                                                        
+   * 用户点击右上角分享                                                                        
    */
   onShareAppMessage: function () {},
 
-  get_num_img: function(num){
+  get_num_img: function (num) {
     get_num_img(num);
   }
 });
 
-/**
- * 建立随机的初始棋盘
- * @param {Number} n 有 n 个有效的格子
- * @param {Number} size 总共有 size 个格子
- * @returns {Number[]} 包含`n`个不同的正整数和`size-n`个`-1`的一维数组
+/**                                                                        
+ * 建立随机的初始棋盘                                                                        
+ * @param {Number} n 有 n 个有效的格子                                                                        
+ * @param {Number} size 总共有 size 个格子                                                                        
+ * @returns {Number[]} 包含`n`个不同的正整数和`size-n`个`-1`的一维数组                                                                        
  */
 function get_Random_board(n, size) {
   let base = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -85,21 +85,21 @@ function get_Random_board(n, size) {
   return out;
 }
 
-/**
- * 打乱一个数组（原地打乱）
- * @param {Number[]} arr 需要被打乱的数组
+/**                                                                        
+ * 打乱一个数组（原地打乱）                                                                        
+ * @param {Number[]} arr 需要被打乱的数组                                                                        
  */
 function randArr(arr) {
   for (var i = 0; i < arr.length; i++) {
     var iRand = parseInt(arr.length * Math.random());
-    [arr[i],arr[iRand]]=[arr[iRand],arr[i]]
+    [arr[i], arr[iRand]] = [arr[iRand], arr[i]]
   }
 }
 
-/**
- * 得到指定数字的图片路径
- * @param {Number} num 需要图片的数字为`num`
- * @returns {String} 返回图片的路径（相对路径）
+/**                                                                        
+ * 得到指定数字的图片路径                                                                        
+ * @param {Number} num 需要图片的数字为`num`                                                                        
+ * @returns {String} 返回图片的路径（相对路径）                                                                        
  */
 function get_num_img(num) {
   console.log("get num", num);
@@ -110,52 +110,66 @@ function get_num_img(num) {
   }
 }
 
-/**
- * 递归地整理棋盘，是棋子聚拢向中间
- * @param {Number[]} board 需要被聚拢的棋盘数组（原地聚拢）
+/**                                                                        
+ * 递归地整理棋盘，是棋子聚拢向中间                                                                        
+ * @param {Number[]} board 需要被聚拢的棋盘数组（原地聚拢）                                                                        
  */
-function prettyBoard(board){
+function prettyBoard(board) {
 
-  /**
-   * 检查棋盘数组是否足够聚拢
-   * @param {Number[]} board 被检查的棋盘数组
-   * @returns {Boolean} 返回`True` or `False`
+  /**                                                                        
+   * 检查棋盘数组是否足够聚拢                                                                        
+   * @param {Number[]} board 被检查的棋盘数组                                                                        
+   * @returns {Boolean} 返回`True` or `False`                                                                        
    */
-  function isOK(board){
-    let start=-1;
-    let totalCnt=0;
-    let stack=[];
-    let isVisited = [];
-    let step_cnt=0;
+  function isOK(board) {
+    let start = -1;
+    let totalCnt = 0;
+    let stack = []; /* 预备查看的索引值 栈 */
+    let isVisited = [];/* -1:无需访问 0:已经访问 >0:需要访问还未访问 */
+    let step_cnt = 0;
 
-    for (let i=0;i<board.length;i++){
-      if(board[i]!=-1){
+    /* 遍历出所有合法点个数 */
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] != -1) {
         totalCnt++;
-        if (start==-1){
-          start=i;
+        /* 保存第一个合法点的下标 */
+        if (start == -1) {
+          start = i;
         }
       }
       isVisited.push(board[i]);
     }
-    stack.push(board[start]);
-    do{
-      let x=stack.pop();
-      
-    }while(step_cnt<totalCnt);
+    stack.push(start);
+    do {
+      let x = stack.pop();/* 取出并保存本次局部搜索的起点索引 */
+      let tars=[x-1,x+1,x-4,x+4];/* 本次局部搜索四个方向上的索引 */
 
+      tars.forEach((tar)=>{
+        /* 如果坐标未越界 */
+        if (tar >= 0 || tar < board.length) {
+          /* 当前节点待遍历 */
+          if(isVisited[tar]>0){
+            isVisited[tar] = 0;/* 标记为已经遍历 */
+            step_cnt++;/* 有效值计数器自增 */
+            stack.push(tar);/* 当前位置标记为下一个起点 */
+          }/* 否则（≤0），无需访问 */
+        }
+      })
+    } while (stack.length>0);
+    return step_cnt==totalCnt;
   }
 
-  /**
-   * 调整`i`号位的棋子
-   * @param {Number} i 这一步想调整的棋子的索引值
+  /**                                                                        
+   * 调整`i`号位的棋子                                                                        
+   * @param {Number} i 这一步想调整的棋子的索引值                                                                        
    */
-  function step(i){
-    if([5,6,9,10].indexOf(i) > -1){
+  function step(i) {
+    if ([5, 6, 9, 10].indexOf(i) > -1) {
 
     }
   }
-  
-  for(let i=0;i<board.length;i++){
+
+  for (let i = 0; i < board.length; i++) {
     step(i);
   }
 }
