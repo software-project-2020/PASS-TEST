133e50356cc8c072aad36264b0ff6724ca94414f
JacksonLee <1602113077@qq.com>
2020-12-15 19:01:57 +0800

    排行榜界面

---
+++ wxapp/utils/rankutil.js
@@ -0,0 +1,25 @@
+module.exports = {
+  getRanklists: getRanklists
+}
+// 获得排行榜数据
+function getRanklists(openid, age, type,page_num,list_num,callback) {
+  wx.request({
+    method: 'POST',
+    dataType: 'json',
+    url: 'https://api.zghy.xyz/api/test/detail',
+    header: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    data: {
+      openid: openid,
+      age: age,
+      type: type,
+      page_num: page_num,
+      list_num: list_num
+    },
+    success: function (res) {
+      // console.log(res.data)
+      callback && callback(JSON.parse(res.data))
+    }
+  })
+}
