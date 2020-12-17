e1ed994d9ecbf84465052dcd89cf732c755e3949
jasmine <1420775889@qq.com>
2020-12-17 15:02:59 +0800

    完善注意游戏

--- wxapp/pages/attention/game2/attention_game.js
+++ wxapp/pages/attention/game2/attention_game.js
@@ -158,7 +158,7 @@ Page({
       var l = this.data.l;
       //保证至少有一个答案
       var place = [];
-      var size = Math.floor((Math.random() * 5) + 3);
+      var size = Math.floor((Math.random() * 10) + 10);
       for (i = 0; i < size; i++) {
         place[i] = Math.floor(((Math.random() * (this.data.line[this.data.number] * this.data.column[this.data.number]))));
       }
