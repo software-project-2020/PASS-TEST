39a3c4b41c979ec8e7d8698887c037b18a284ecc
SevDaisy <1052886775@qq.com>
2020-12-25 15:20:43 +0800

    dev

--- wxapp/pages/zyy_memory_span/rule/rule.js
+++ wxapp/pages/zyy_memory_span/rule/rule.js
@@ -1,5 +1,35 @@
 // pages/zyy_memory_span/rule/rule.js
 Page({
+  data: {
+    numList: [{
+      name: '开始'
+    }, {
+      name: '画图'
+    }, {
+      name: '错误'
+    }, {
+      name: '完成'
+    }, ],
+    num: 0,
+    scroll: 0,
+    rules: [{
+        words: "进入页面后，你将会看到一句描述性的文字",
+        img: "https://picture.morii.top/renzhixuetang/rules/S11-rule/S11-step1.jpg"
+      },
+      {
+        words: "你需要将题目中描述的形状在屏幕上画出来",
+        img: "https://picture.morii.top/renzhixuetang/rules/S11-rule/S11-step2.gif"
+      },
+      {
+        words: "请注意，为避免误判，请尽量将图形画的标准一些哦",
+        img: "https://picture.morii.top/renzhixuetang/rules/S11-rule/S11-step3.gif"
+      },
+      {
+        words: "做完后请点击下面的按钮提交成绩。如果已经对规则了解了的话，就点击开始测试的按钮吧",
+        img: "https://picture.morii.top/renzhixuetang/rules/S11-rule/S11-step4.jpg"
+      }
+    ]
+  },
   onShow: function () {
     wx.setNavigationBarTitle({
       title: '继时性加工测试'
@@ -24,4 +54,14 @@ Page({
       }
     });
   },
+  numSteps() {
+    this.setData({
+      num: this.data.num == this.data.numList.length - 1 ? 0 : this.data.num + 1
+    })
+  },
+  numStepsBack() {
+    this.setData({
+      num: this.data.num - 1
+    })
+  }
 });
