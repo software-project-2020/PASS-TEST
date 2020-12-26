60d26dd87ee3664c674a743af56230a7f0e0ae4f
Cadelle1 <1024670967@qq.com>
2020-12-26 16:17:39 +0800

    规则页弹窗

--- wxapp/pages/planning-test/rule1/rule1.js
+++ wxapp/pages/planning-test/rule1/rule1.js
@@ -36,19 +36,19 @@ Page({
   },
   gototest1: function (e) {
 
-    wx.showModal({
-      title: '开始练习',
-      content: '在开始测试之前，你有一次练习的机会，练习将不会被计入成绩，快去熟悉一下题目吧!',
-      cancelText:'取消',
-      confirmText:'开始练习',
-      success: function (res) {
-        if (res.confirm) {//这里是点击了确定以后
+    // wx.showModal({
+    //   title: '开始练习',
+    //   content: '在开始测试之前，你有一次练习的机会，练习将不会被计入成绩，快去熟悉一下题目吧!',
+    //   cancelText:'取消',
+    //   confirmText:'开始练习',
+    //   success: function (res) {
+    //     if (res.confirm) {//这里是点击了确定以后
           wx.redirectTo({
             url: '/pages/planning-test/test1/test1',
           })
-        }
-      }
-    })
+    //     }
+    //   }
+    // })
   },
 
   /**
