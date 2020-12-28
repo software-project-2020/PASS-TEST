6922ddec2cbb05c55df815e17d5deb755b8e7718
zlx <2780311834@qq.com>
2020-12-28 13:39:57 +0800

    修改练习为尝试

--- wxapp/pages/zyy_memory_span/rule/rule.js
+++ wxapp/pages/zyy_memory_span/rule/rule.js
@@ -37,10 +37,10 @@ Page({
   },
   goTest: function (e) {
     wx.showModal({
-      title: '开始练习',
-      content: '在开始测试之前，你有一次练习的机会，快去熟悉一下题目吧!',
-      cancelText: '跳过练习',
-      confirmText: '练习一下',
+      title: '开始尝试',
+      content: '在开始尝试之前，你有一次练习的机会，快去熟悉一下题目吧!',
+      cancelText: '跳过尝试',
+      confirmText: '尝试一下',
       success: function (res) {
         if (res.confirm) { //这里是点击了确定以后
           wx.redirectTo({
